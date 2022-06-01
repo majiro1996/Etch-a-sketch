bacb705be928fb703ae364f434a548138b2eef29
Majiro <majiro1996@gmail.com>
2022-06-01 19:00:35 +0200

    Rework makeGrid function

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-//
-function makePixel(height,width){
+//varibles
+let pSize=27
+let canSize=432
+let quantity=16
+makeGrid(16,16);
+function makeGrid(height,width){
     let container=document.querySelector("#container");
-    for(let i=0;i<(height*width);i++){
+    for(let i=1;i<=(height*width);i++){
         let square=document.createElement("div");
         square.classList.add("squares")
         container.appendChild(square);
+
     }
     
 }
 
-makePixel(16,16);
+/*function removePixel(canvas){
+    while (canvas.firstChild){
+        canvas.removeChild(canvas.firstChild);
+    }
+}*/
+
+window.addEventListener("click")
+
+
+
+
+
+
+
